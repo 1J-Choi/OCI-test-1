63fc47586a6f3dbc7c3f637238df1f5ff95fa383
1J-Choi <chldnjswp110@naver.com>
2025-10-29 16:25:49 +0900

    deploy.yml 수정

--- src/main/java/com/back/HomeController.java
+++ src/main/java/com/back/HomeController.java
@@ -10,6 +10,7 @@ public class HomeController {
         return """
                 <h1>Welcome to the Home Page!</h1>
                 <p>This is a Demo Spring Boot application.</p>
+                <p>나 최원제는 Oracle Cloud를 통해 배포를 할 수 있다!</p>
                 """;
     }
 }
